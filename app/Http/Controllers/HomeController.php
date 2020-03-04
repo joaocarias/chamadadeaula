6d6d7ac77292218410eb24a33622322849a8df9e
joao.carias <ti.joao@liga.org.br>
2020-03-04 14:07:28 -0300

    exibindo alunos na home principal

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,27 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Aluno;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
-{
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
+{    
     public function __construct()
     {
         $this->middleware('auth');
     }
-
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
+  
     public function index()
     {
-        return view('home');
+        $alunos = Aluno::orderBy('created_at', 'desc')->take(5)->get();
+        return view('home', ['alunos' => $alunos]);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,26 +1,66 @@
+<?php
+$permissoes = array();
+if (isset(Auth::user()->regras)) {
+    foreach (Auth::user()->regras as $regra) {
+        array_push($permissoes, $regra->nome);
+    }
+}
+?>
+
 @extends('layouts.app')
 
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-12">
+            @if (session('status'))
             <div class="card">
-                <div class="card-header">Dashboard</div>
-
+                <div class="card-header">Alerta</div>
                 <div class="card-body">
-                    @if (session('status'))
+
                     <div class="alert alert-success" role="alert">
                         {{ session('status') }}
                     </div>
-                    @endif
 
-                    You are logged in!
+                </div>
+            </div>
+            @endif
+        </div>
+    </div>
 
-                    
+    @if(isset($alunos) && !is_null($alunos) && count($alunos) > 0 && in_array("ADMINISTRADOR", $permissoes))
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card mb-3">
+                <div class="card-header">{{ __('Últimos Alunos Cadastrados') }}</div>
+                <div class="card-body">
+                    <table class="table table-hover">
+                        <thead>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Nome</th>
+                                <th scope="col"></th>
+                            <tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($alunos as $item)
+                            <tr>
+                                <td scope="row">{{ __($item->id) }}</td>
+                                <td>{{ __($item->nome) }}</td>
+                                <td class="text-right">
+                                    <a href="{{ route('exibir_aluno', [$item->id]) }}" class="btn btn-dark btn-sm"><i class="far fa-folder-open"></i> &nbsp; Detalhes</a>
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+
                 </div>
             </div>
         </div>
     </div>
+    @endif
+
 </div>
 
 @endsection
